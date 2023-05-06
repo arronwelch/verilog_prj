583864cdab3e0d62cdc41224cea8650882784d02
nick <Davego@protonmail.com>
2023-05-06 23:57:41 +0800

    concatenation operator for vector

--- 2 Verilog Language/2.2 Vectors/14_vectorgates.v
+++ 2 Verilog Language/2.2 Vectors/14_vectorgates.v
@@ -1,4 +1,6 @@
-module top_module( 
+// 14_vectorgates.v
+
+module top_module ( 
     input [2:0] a,
     input [2:0] b,
     output [2:0] out_or_bitwise,
@@ -7,7 +9,8 @@ module top_module(
 );
     assign out_or_bitwise = a|b;
     assign out_or_logical = a||b;
-    assign out_not[5:3] = ~b;
-    assign out_not[2:0] = ~a;
+    // assign out_not[5:3] = ~b;
+    // assign out_not[2:0] = ~a;
+    assign out_not = { ~b, ~a };
 
 endmodule
--- 2 Verilog Language/2.2 Vectors/16_vector3.v
+++ 2 Verilog Language/2.2 Vectors/16_vector3.v
@@ -1,3 +1,6 @@
+// 16_vector3.v
+// Part selection
+
 module top_module (
     input [4:0] a, b, c, d, e, f,
     output [7:0] w, x, y, z );

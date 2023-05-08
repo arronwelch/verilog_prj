b58db2c48f4e04309b16832b73939b510d93eaa9
nick <Davego@protonmail.com>
2023-05-09 00:39:41 +0800

    use a loop to creates combinational logic

--- 2 Verilog Language/2.2 Vectors/16_vector3.v
+++ 2 Verilog Language/2.2 Vectors/16_vector3.v
@@ -1,5 +1,22 @@
-// 16_vector3.v
-// Part selection
+/* 16_vector3.v
+ * concatenation operator
+ * {3'b111, 3'b000} => 6'b111000
+ * {1'b1, 1'b0, 3'b101} => 5'b10101
+ * {4'ha, 4'd10} => 8'b10101010 // 4'ba and 4'b10 are both 4'b1010 in binary
+ * Concatenation needs to know the width of every component
+ * (or how would you know the length of the result?)
+ * The concatenation operator can be used on both the left and right sides of assignments.
+ * input [15:0] in;
+ * output [23:0] out;
+ * assign {out[7:0], out[15:8]} = in;   // Swap two bytes. Right and left
+ *                                      // side are both 16-bit vectors.
+ * assign out[15:0] = {in[7:0], in[15:8]};  // This is the same thing.
+ * assign out = {in[7:0], in[15:8]};    // This is different. The 16-bit vector
+ *                                      // on the right is extended to match the
+ *                                      // 24-bit vector on the left, so out[23:16]
+ *                                      // are zero.
+ * // In the first two examples, out[23:16] are not assigned.
+ */
 
 module top_module (
     input [4:0] a, b, c, d, e, f,
--- 2 Verilog Language/2.2 Vectors/17_vectorr.v
+++ 2 Verilog Language/2.2 Vectors/17_vectorr.v
@@ -1,3 +1,6 @@
+/* 17_vectorr.v
+ * Given an 8-bit input vector[7:0], reverse its bit ordering.
+ */
 module top_module( 
     input [7:0] in,
     output [7:0] out
@@ -6,19 +9,7 @@ module top_module(
 
     //assign {out[0],out[1],out[2],out[3],out[4],out[5],out[6],out[7]} = in;
 
-    // always @(*) begin	
-	// for (int i=0; i<8; i++)	// int is a SystemVerilog type. Use integer for pure Verilog.
-	// 		out[i] = in[8-i-1];
-	// end
-
-	generate
-		genvar i;
-		for (i=0; i<8; i = i+1) begin: my_block_name
-			assign out[i] = in[8-i-1];
-		end
-	endgenerate
-
-	/*
+	//
 	// I know you're dying to know how to use a loop to do this:
 
 	// Create a combinational always block. This creates combinational logic that computes the same result
@@ -51,6 +42,6 @@ module top_module(
 			assign out[i] = in[8-i-1];
 		end
 	endgenerate
-	*/
+	//
 
 endmodule

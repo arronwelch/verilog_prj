f7fe607a6a6f2aea9932b7b716a56229551d412b
nick <Davego@protonmail.com>
2023-06-09 00:05:30 +0800

    format file coding style

--- 2_Verilog_Language/2_3_Modules_Hierarchy/27_module_cseladd.v
+++ 2_Verilog_Language/2_3_Modules_Hierarchy/27_module_cseladd.v
@@ -6,18 +6,18 @@ module top_module(
     input [31:0] b,
     output [31:0] sum
 );
-    reg cout1,cout2,cout3;
-    reg[15:0] sum_h0,sum_h1;
-    add16 add16_l(a[15:0],b[15:0],0,sum[15:0],cout1);
-    add16 add16_ci0(a[31:16],b[31:16],0,sum_h0,cout2);
-    add16 add16_ci1(a[31:16],b[31:16],1,sum_h1,cout3);
+    reg cout1, cout2, cout3;
+    reg[15:0] sum_h0, sum_h1;
+    add16 add16_l(a[15:0], b[15:0], 0, sum[15:0], cout1);
+    add16 add16_ci0(a[31:16], b[31:16], 0, sum_h0, cout2);
+    add16 add16_ci1(a[31:16], b[31:16], 1, sum_h1, cout3);
     always @(*) begin
         case(cout1)
-            1'b0:sum[31:16] = sum_h0;
-            1'b1:sum[31:16] = sum_h1;
+            1'b0 : sum[31:16] = sum_h0;
+            1'b1 : sum[31:16] = sum_h1;
             default:
-                sum[31:16] = 16'hxx; 
+                sum[31:16] = 16'hxx;
         endcase
     end
-
 endmodule
+

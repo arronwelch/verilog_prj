94159b9a945a1d6aae7aad5afe0154a98e6f601c
nick <Davego@protonmail.com>
2023-06-13 23:57:18 +0800

    the first always block

---
+++ 2_Verilog_Language/2_4_Procedures/29_alwaysblock1.v
@@ -0,0 +1,16 @@
+// 29_alwaysblock1.v
+
+// synthesis verilog_input_version verilog_2001
+module top_module(
+    input a, 
+    input b,
+    output wire out_assign,
+    output reg out_alwaysblock
+);
+    assign out_assign = a & b;
+
+    always @(*) begin
+        out_alwaysblock = a & b;
+    end
+
+endmodule
